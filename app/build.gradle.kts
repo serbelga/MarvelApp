70a4692c6b45821a2343e48a1315a0797760f16d
Sergio Belda <sbeldagalbis@gmail.com>
2023-05-25 22:30:07 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,11 +26,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(17)
     }
     kotlinOptions {
-        jvmTarget = "11"
         freeCompilerArgs += "-Xjvm-default=all"
     }
     buildFeatures {
